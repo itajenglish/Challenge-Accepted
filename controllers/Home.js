e5b48ee416112620e1aefa2dbc38d9598e0fbd55
Taj English <tajenglish@gmail.com>
2017-03-11 12:18:03 -0500

    home page now renders

--- controllers/Home.js
+++ controllers/Home.js
@@ -1,26 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllDjs } = require('../models/Home');
-
-//Checks for user session and what type of user you are to show currect view.
-router.all('/', (req, res, next) => {
-  const user = req.session.user;
-  console.log(user)
-  if (user) {
-    if (user.type === "fan") {
-      res.redirect('/userboard');
-    } else if (user.type === "dj") {
-      res.redirect("/djboard");
-    }
-  } else {
-    next();
-  }
-})
 
 // Home#Show
-router.get('/', getAllDjs, (req, res, next) => {
-    const djs = req.user;
-    res.render('home/index', { djs });
+router.get('/', (req, res, next) => {
+    res.render('home/index');
 });
 
 module.exports = router;
