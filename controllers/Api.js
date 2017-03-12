d823d734eccbf217cd27862d3dd3e7abb8f5ddd2
Taj English <tajenglish@gmail.com>
2017-03-11 19:16:37 -0500

    update

--- controllers/Api.js
+++ controllers/Api.js
@@ -8,7 +8,9 @@ router.post('/users', register, (req, res, next) => {
   res.send('Success!');
 });
 
-router.get('/videos', getAllVideos, (req, res, next) => {})
+router.get('/videos', getAllVideos, (req, res, next) => {
+  console.log(req);
+})
 
 router.post('/videos', addNewVideo, (req, res, next) => {})
 
